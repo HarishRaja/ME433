25875b29c3a507b94b0ad5c9f3f9ea74d8521608
HarishRaja <hraja@dhcp-10-101-58-102.wireless.northwestern.private>
2015-04-15 17:16:47 -0500

    Accelerometer on OLED

---
+++ Accelerometer/Accelerometer.X/build/default/production/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/main.o:  \
+main.c  \
+i2c_master_int.h  \
+i2c_display.h  \
+ accel.h 
---
+++ Accelerometer/Accelerometer.X/main.c
@@ -0,0 +1,370 @@
+/* 
+ * File:   main.c
+ * Author: hraja
+ *
+ * Created on April 13, 2015, 2:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "i2c_master_int.h"
+#include "i2c_display.h"
+#include "accel.h"
+
+#include<xc.h> // processor SFR definitions
+#include<sys/attribs.h> // __ISR macro
+
+// DEVCFG0
+#pragma config DEBUG = 0 // no debugging
+#pragma config JTAGEN = 0 // no jtag
+#pragma config ICESEL = 0b11 // use PGED1 and PGEC1
+#pragma config PWP = 0b111111111 // no write protect
+#pragma config BWP = 1 // not boot write protect
+#pragma config CP = 1 // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = 0b011 // use primary oscillator with pll
+#pragma config FSOSCEN = 0 // turn off secondary oscillator
+#pragma config IESO = 0 // no switching clocks
+#pragma config POSCMOD = 0b10 // high speed crystal mode
+#pragma config OSCIOFNC = 1 // free up secondary osc pins
+#pragma config FPBDIV = 0b00 // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = 0b11 // do not enable clock switch
+#pragma config WDTPS = 0b00000 // slowest wdt
+#pragma config WINDIS = 1 // no wdt window
+#pragma config FWDTEN = 0 // wdt off by default
+#pragma config FWDTWINSZ = 0b11 // wdt window at 25%
+
+// DEVCFG2 - get the CPU clock to 40MHz
+#pragma config FPLLIDIV = 0b001 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = 0b101 // multiply clock after FPLLIDIV
+#pragma config UPLLIDIV = 0b001 // divide clock after FPLLMUL
+#pragma config UPLLEN = ON // USB clock on
+#pragma config FPLLODIV = DIV_2 // divide clock by 1 to output on pin
+
+// DEVCFG3
+#pragma config USERID = 0xFFFF // some 16bit userid
+#pragma config PMDL1WAY = 1 // not multiple reconfiguration, check this
+#pragma config IOL1WAY = 1// not multimple reconfiguration, check this
+#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
+#pragma config FVBUSONIO = 1 // controlled by USB module
+
+
+#define length 100
+
+short accels[3]; // accelerations for the 3 axes
+short mags[3]; // magnetometer readings for the 3 axes
+short temp;
+char buffer[length];
+static const char ASCII[96][5] = {
+ {0x00, 0x00, 0x00, 0x00, 0x00} // 20  (space)
+,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
+,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
+,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
+,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
+,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
+,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
+,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
+,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
+,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
+,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
+,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
+,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
+,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
+,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
+,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
+,{0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
+,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
+,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
+,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
+,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
+,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
+,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
+,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
+,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
+,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
+,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
+,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
+,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
+,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
+,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
+,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
+,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
+,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
+,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
+,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
+,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
+,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
+,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
+,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
+,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
+,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
+,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
+,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
+,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
+,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
+,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
+,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
+,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
+,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
+,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
+,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
+,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
+,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
+,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
+,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
+,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
+,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
+,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
+,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
+,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
+,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
+,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
+,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
+,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
+,{0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
+,{0x7f, 0x48, 0x44, 0x44, 0x38} // 62 b
+,{0x38, 0x44, 0x44, 0x44, 0x20} // 63 c
+,{0x38, 0x44, 0x44, 0x48, 0x7f} // 64 d
+,{0x38, 0x54, 0x54, 0x54, 0x18} // 65 e
+,{0x08, 0x7e, 0x09, 0x01, 0x02} // 66 f
+,{0x0c, 0x52, 0x52, 0x52, 0x3e} // 67 g
+,{0x7f, 0x08, 0x04, 0x04, 0x78} // 68 h
+,{0x00, 0x44, 0x7d, 0x40, 0x00} // 69 i
+,{0x20, 0x40, 0x44, 0x3d, 0x00} // 6a j
+,{0x7f, 0x10, 0x28, 0x44, 0x00} // 6b k
+,{0x00, 0x41, 0x7f, 0x40, 0x00} // 6c l
+,{0x7c, 0x04, 0x18, 0x04, 0x78} // 6d m
+,{0x7c, 0x08, 0x04, 0x04, 0x78} // 6e n
+,{0x38, 0x44, 0x44, 0x44, 0x38} // 6f o
+,{0x7c, 0x14, 0x14, 0x14, 0x08} // 70 p
+,{0x08, 0x14, 0x14, 0x18, 0x7c} // 71 q
+,{0x7c, 0x08, 0x04, 0x04, 0x08} // 72 r
+,{0x48, 0x54, 0x54, 0x54, 0x20} // 73 s
+,{0x04, 0x3f, 0x44, 0x40, 0x20} // 74 t
+,{0x3c, 0x40, 0x40, 0x20, 0x7c} // 75 u
+,{0x1c, 0x20, 0x40, 0x20, 0x1c} // 76 v
+,{0x3c, 0x40, 0x30, 0x40, 0x3c} // 77 w
+,{0x44, 0x28, 0x10, 0x28, 0x44} // 78 x
+,{0x0c, 0x50, 0x50, 0x50, 0x3c} // 79 y
+,{0x44, 0x64, 0x54, 0x4c, 0x44} // 7a z
+,{0x00, 0x08, 0x36, 0x41, 0x00} // 7b {
+,{0x00, 0x00, 0x7f, 0x00, 0x00} // 7c |
+,{0x00, 0x41, 0x36, 0x08, 0x00} // 7d }
+,{0x10, 0x08, 0x08, 0x10, 0x08} // 7e ?
+,{0x00, 0x06, 0x09, 0x09, 0x06} // 7f ?
+}; // end char ASCII[96][5]
+
+
+
+int readADC(void);
+
+int main() {
+
+    // startup
+
+
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that
+    // kseg0 is cacheable (0x3) or uncacheable (0x2)
+    // see Chapter 2 "CPU for Devices with M4K Core"
+    // of the PIC32 reference manual
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // no cache on this chip!
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to be able to use TDI, TDO, TCK, TMS as digital
+    DDPCONbits.JTAGEN = 0;
+
+    __builtin_enable_interrupts();
+
+    // set up USER pin as input
+    ANSELBbits.ANSB13 = 0;
+    ANSELBbits.ANSB15 = 0;
+    TRISBbits.TRISB13=1;
+
+    // set up LED1 pin as a digital output
+    TRISBbits.TRISB7 = 0;
+    int light = 1;
+    LATBbits.LATB7 = light;
+
+
+
+    RPB15Rbits.RPB15R  = 0b0101;
+
+    // set up LED2 as OC1 using Timer2 at 1kHz
+
+    T2CONbits.TCKPS = 0;
+    T2CONbits.TCS = 0;
+    PR2 = 9999;
+    TMR2 = 0;
+
+    OC1CONbits.OCTSEL = 0;
+    OC1CONbits.OCM = 0b110;
+    OC1RS = 5000;
+    OC1R = 5000;
+
+    T2CONbits.ON  = 1;
+    OC1CONbits.ON = 1;
+
+    // set up A0 as AN0
+    ANSELAbits.ANSA0 = 1;
+    AD1CON3bits.ADCS = 3;
+    AD1CHSbits.CH0SA = 0;
+    AD1CON1bits.ADON = 1;
+
+    _CP0_SET_COUNT(0);
+
+    display_init();
+    int number = 1337;
+    sprintf(buffer,"Hello World %d!", number);
+
+    acc_setup();
+
+    //write_to_display(10,10,buffer);
+
+
+    while (1) {
+        // invert pin every 0.5s, set PWM duty cycle % to the pot voltage output %
+        read_accel();
+        int pot = readADC();
+        OC1RS = pot*9;
+
+        if(PORTBbits.RB13 == 0)
+        {
+            LATBbits.LATB7 = !LATBbits.LATB7;
+        }
+
+        if(_CP0_GET_COUNT()>=1000000)
+        {
+            light = !light;
+            LATBbits.LATB7 = light;
+            _CP0_SET_COUNT(0);
+        }
+    }
+}
+
+int readADC(void) {
+    int elapsed = 0;
+    int finishtime = 0;
+    int sampletime = 20;
+    int a = 0;
+
+    AD1CON1bits.SAMP = 1;
+    elapsed = _CP0_GET_COUNT();
+    finishtime = elapsed + sampletime;
+    while (_CP0_GET_COUNT() < finishtime) {
+    }
+    AD1CON1bits.SAMP = 0;
+    while (!AD1CON1bits.DONE) {
+    }
+    a = ADC1BUF0;
+    return a;
+}
+
+void write_to_display(int xstart, int ystart, char text[]){
+    int i = 0;
+    int ascii_byte;
+    int j;
+    int k;
+    int x;
+    int y;
+
+    x = xstart;
+    y = ystart;
+
+    while(text[i]){
+        for (j = 0; j<5; j++){
+           ascii_byte = ASCII[text[i]-0x20][j];
+           k=0;
+           for(k=0;k<8;k++){
+               if(x <129 && y<64){
+                    display_pixel_set(y,x,ascii_byte&1);
+                    ascii_byte = ascii_byte>>1;
+               }
+               y++;
+               if (k == 7){
+                    y=ystart;
+               }
+               
+           }
+           x++;
+        }
+        i++;
+    }
+
+    display_draw();
+}
+
+void read_accel(){
+    display_clear();
+    acc_read_register(OUT_X_L_A, (unsigned char *) accels, 6);
+    sprintf(buffer,"x: %d, y: %d, z: %d",accels[0],accels[1],accels[2]);
+    //write_to_display(10,10,buffer);
+
+     int sizex;
+     int sizey;
+     int i;
+     int j;
+     int flagx = 1;
+     int flagy = 1;
+
+     sizex = accels[0]/1000;
+     sizey = accels[1]/700;
+
+     if(sizex<0){
+         sizex = sizex * -1;
+         flagx = 0;
+     }
+
+     if(sizey<0){
+         sizey = sizey * -1;
+         flagy = 0;
+     }
+
+     for (i=1;i<=sizex;i++){
+         if (!flagx){
+            display_pixel_set(31,65+i,1);
+            display_pixel_set(32,65+i,1);
+            display_pixel_set(33,65+i,1);
+         }
+         else{
+            display_pixel_set(33,63-i,1);
+            display_pixel_set(32,63-i,1);
+            display_pixel_set(31,63-i,1);
+         }
+     }
+
+     for (j=1;j<=sizey;j++){
+         if (!flagy){
+            display_pixel_set(33+j,63,1);
+            display_pixel_set(33+j,64,1);
+            display_pixel_set(33+j,65,1);
+         }
+         else{
+            display_pixel_set(31-j,63,1);
+            display_pixel_set(31-j,64,1);
+            display_pixel_set(31-j,65,1);
+         }
+     }
+
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+            display_pixel_set(31+i,63+j,1);
+         }
+     }
+
+     display_draw();
+     
+
+
+}
